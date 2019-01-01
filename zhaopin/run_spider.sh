6adff8d40385918b6a72c9e95393368d92b444e2
jianfeng.wang <jianfeng.wang@maixunbytes.com>
2019-01-01 09:49:15 +0800

    change some

--- zhaopin/run_spider.sh
+++ zhaopin/run_spider.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-ps -few | grep "srcapy crawl $1" | grep -v grep | awk '{print $2}' | xargs kill -9
-nohup srcapy crawl $1  > ~/log/$1.log 2>&1 &
+ps -few | grep "scrapy crawl $1" | grep -v grep | awk '{print $2}' | xargs kill -9
+nohup ~/anaconda3/bin/scrapy crawl $1  > ~/log/$1.log 2>&1 &
